c761f4b2abc20071dbe8bfc3af0c4b69a3bf7e1f
Vinothkumar R <vinothkumar.r@iouring.com>
2022-05-27 21:43:32 +0530

    Add support for optional authenticators

---
+++ local_auth_ios/example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,28 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<integration_test/IntegrationTestPlugin.h>)
+#import <integration_test/IntegrationTestPlugin.h>
+#else
+@import integration_test;
+#endif
+
+#if __has_include(<local_auth_ios/FLTLocalAuthPlugin.h>)
+#import <local_auth_ios/FLTLocalAuthPlugin.h>
+#else
+@import local_auth_ios;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [IntegrationTestPlugin registerWithRegistrar:[registry registrarForPlugin:@"IntegrationTestPlugin"]];
+  [FLTLocalAuthPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTLocalAuthPlugin"]];
+}
+
+@end
---
+++ local_auth_platform_interface/lib/types/authenticator.dart
@@ -0,0 +1,15 @@
+enum Authenticator {
+  /// Any biometric (e.g. fingerprint, iris, or face) on the device that the
+  /// platform API considers to be strong. For example, on Android this
+  /// corresponds to Class 3.
+  strong('strong'),
+
+  /// Any biometric (e.g. fingerprint, iris, or face) on the device that the
+  /// platform API considers to be weak. For example, on Android this
+  /// corresponds to Class 2.
+  weak('weak');
+
+  final String value;
+
+  const Authenticator(this.value);
+}
